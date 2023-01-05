9563a32c8840de48359690ef56fabb5d55e7aea1
drugstoreworker <drugstoreworker@gmail.com>
2023-01-05 15:32:57 +0600

    fix: conflicts

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage/HomePage';
 import LoginPage from './pages/LoginPage/LoginPage';
+import './App.scss';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<HomePage/>}/>
         <Route path='/login' element={<LoginPage/>}/>
       </Routes>
+      <h1>Hello world</h1>
     </div>
   );
 }
