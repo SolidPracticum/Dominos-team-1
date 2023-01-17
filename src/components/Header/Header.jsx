c436ab43ea8c691c1958799f0a232e37d6c27b90
drugstoreworker <drugstoreworker@gmail.com>
2023-01-17 17:03:43 +0600

    feat: adaptive layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -34,12 +34,14 @@ const Header = () => {
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Domino%27s_pizza_logo.svg/2036px-Domino%27s_pizza_logo.svg.png" alt="" />
             <h1>Domino's Pizza</h1>
         </Link>
-
-        <Link to="/promo">Promo</Link>
+      <div className={scss.food}>
+      <Link to="/promo">Promo</Link>
         <Link to="/pizza">Pizza</Link>
         <Link to="/drinks">Drinks</Link>
         <Link to="/sides">Sides</Link>
         <Link to="/dessert">Dessert</Link>
+      </div>
+       
 
         <div className={scss.basket}>
             <div>
@@ -47,7 +49,7 @@ const Header = () => {
             </div>
             <button>Checkout</button>
         </div>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Hamburger_icon_white.svg/1024px-Hamburger_icon_white.svg.png" alt="" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Hamburger_icon_white.svg/1024px-Hamburger_icon_white.svg.png" alt="" id={scss.pic} />
       </div>
 
 
